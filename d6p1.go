11247be080086aa99ba7fa4ed333d0e074ad6b45
Trond Isak Alseth <t.alseth@sportradar.com>
2018-12-06 15:41:22 +0100

    tmp

--- d6p1.go
+++ d6p1.go
@@ -2,7 +2,9 @@ package main
 
 import (
 	"io/ioutil"
+	"log"
 	"math"
+	"sort"
 	"strconv"
 	"strings"
 )
@@ -19,17 +21,38 @@ func MyAtoi(v string) int {
 
 func shortestId(coords [][]string, x int, y int) int {
 	id := 0
+	dup := 0
 	length := 999
 	for ci, c := range coords {
 		cx := MyAtoi(c[0])
 		cy := MyAtoi(c[1])
-		len := int(math.Abs(float64(cx - x + cy - y)))
-		if len < length {
+		xr := math.Abs(float64(x - cx))
+		yr := math.Abs(float64(y - cy))
+		len := int(xr + yr)
+		if len <= length {
+			if len == length {
+				dup = id
+			} else {
+				dup = 0
+			}
 			length = len
 			id = ci
 		}
 	}
-	return id
+	if dup != 0 {
+		return 99
+	} else {
+		return id
+	}
+}
+
+func Add(v int, a []int) []int {
+	for _, b := range a {
+		if v == b {
+			return a
+		}
+	}
+	return append(a, v)
 }
 
 func main() {
@@ -54,18 +77,31 @@ func main() {
 	for x := 0; x < 500; x++ {
 		y := 0
 		shortest := shortestId(coords, x, y)
-		if _, !ok := inflist[shortest] {
-			inflist[]
-		}
+		inflist = Add(shortest, inflist)
 		y = 500
 		shortest = shortestId(coords, x, y)
+		inflist = Add(shortest, inflist)
 	}
-
+	sort.Ints(inflist)
+	log.Println(inflist)
+	counter := make([]int, 100)
 	for i := 0; i < 500; i++ {
 		for y := 0; y < 500; y++ {
-
+			system[i][y] = shortestId(coords, i, y)
+			counter[system[i][y]]++
 		}
 	}
-	//log.Println(system)
+	for hi, h := range counter {
+		found := false
+		for _, g := range inflist {
+			if hi == g {
+				found = true
+			}
+		}
+		if !found {
+			log.Println(hi, h)
+		}
+	}
+	log.Println(counter)
 
 }
