46d3cb313fb51a22e0bc70be2a16e3cf485e4085
Trond Isak Alseth <t.alseth@sportradar.com>
2018-12-13 13:10:09 +0100

    d13. p1+p2 at same time

--- d13p1.go
+++ d13p1.go
@@ -3,8 +3,10 @@ package main
 import (
 	"io/ioutil"
 	"log"
-	"strconv"
+	"os"
+	"sort"
 	"strings"
+	"time"
 )
 
 func check(e error) {
@@ -13,21 +15,23 @@ func check(e error) {
 	}
 }
 
-func MyAtoi(v string) int {
-	c, _ := strconv.Atoi(v)
-	return c
-}
-
 type cart struct {
 	x                  int
 	y                  int
 	mydirection        rune // <>v^
 	lastinterdirection int  // 0 1 2, 0 == left, 1 == straight, 2 == rigth
 }
+type cartSlice []cart
+
+func (c cartSlice) Len() int           { return len(c) }
+func (c cartSlice) Less(i, j int) bool { return c[i].y < c[j].y }
+func (c cartSlice) Swap(i, j int)      { c[i], c[j] = c[j], c[i] }
 
 func main() {
+	t_input := time.Now()
 	b, err := ioutil.ReadFile("d13.data")
 	check(err)
+
 	foo := strings.Split(string(b), "\n")
 	foo = foo[:len(foo)-1]
 	chart := make([][]rune, len(foo))
@@ -35,42 +39,177 @@ func main() {
 		chart[fi] = []rune(f)
 	}
 	carts := make([]cart, 0)
-	for xi, c := range chart {
-		for yi, r := range c {
+	for yi, c := range chart {
+		for xi, r := range c {
 			if r == '<' || r == '>' || r == 'v' || r == '^' {
 				carts = append(carts, cart{x: xi, y: yi, mydirection: r})
+				if r == '<' || r == '>' {
+					chart[yi][xi] = '-'
+				}
+				if r == '^' || r == 'v' {
+					chart[yi][xi] = '|'
+				}
 			}
+
 		}
 	}
+	input_t_elapsed := time.Since(t_input)
+	log.Println("Input:", input_t_elapsed)
+	t_input = time.Now()
 	// ticks
-	for ci, c := range carts {
-		switch chart[c.x][c.y] {
-		case '|':
-			log.Println("continue same dir")
-			if c.mydirection == '^' {
-				chart[c.x][c.y].y--
+	//for k := 0; k < 10000; k++ {
+	firstcoll := false
+	for {
+		sort.Sort(cartSlice(carts))
+		deletionlist := make([]int, 0)
+		for ci, c := range carts {
+			switch chart[c.y][c.x] {
+			case '|':
+				if c.mydirection == '^' {
+					carts[ci].y--
+				}
+				if c.mydirection == 'v' {
+					carts[ci].y++
+				}
+			case '+':
+				switch c.mydirection {
+				case '<':
+					switch c.lastinterdirection {
+					case 0:
+						carts[ci].mydirection = 'v'
+						carts[ci].y++
+						carts[ci].lastinterdirection++
+					case 1:
+						carts[ci].x--
+						carts[ci].lastinterdirection++
+					case 2:
+						carts[ci].mydirection = '^'
+						carts[ci].y--
+						carts[ci].lastinterdirection = 0
+					}
+				case '^':
+					switch c.lastinterdirection {
+					case 0:
+						carts[ci].mydirection = '<'
+						carts[ci].x--
+						carts[ci].lastinterdirection++
+					case 1:
+						carts[ci].y--
+						carts[ci].lastinterdirection++
+					case 2:
+						carts[ci].mydirection = '>'
+						carts[ci].x++
+						carts[ci].lastinterdirection = 0
+					}
+				case '>':
+					switch c.lastinterdirection {
+					case 0:
+						carts[ci].mydirection = '^'
+						carts[ci].y--
+						carts[ci].lastinterdirection++
+					case 1:
+						carts[ci].x++
+						carts[ci].lastinterdirection++
+					case 2:
+						carts[ci].mydirection = 'v'
+						carts[ci].y++
+						carts[ci].lastinterdirection = 0
+					}
+				case 'v':
+					switch c.lastinterdirection {
+					case 0:
+						carts[ci].mydirection = '>'
+						carts[ci].x++
+						carts[ci].lastinterdirection++
+					case 1:
+						carts[ci].y++
+						carts[ci].lastinterdirection++
+					case 2:
+						carts[ci].mydirection = '<'
+						carts[ci].x--
+						carts[ci].lastinterdirection = 0
+					}
+				}
+			case '/':
+				switch c.mydirection {
+				case '<':
+					carts[ci].mydirection = 'v'
+					carts[ci].y++
+				case '^':
+					carts[ci].mydirection = '>'
+					carts[ci].x++
+				case '>':
+					carts[ci].mydirection = '^'
+					carts[ci].y--
+				case 'v':
+					carts[ci].mydirection = '<'
+					carts[ci].x--
+				}
+			case '\\':
+				switch c.mydirection {
+				case '<':
+					carts[ci].mydirection = '^'
+					carts[ci].y--
+				case '^':
+					carts[ci].mydirection = '<'
+					carts[ci].x--
+				case '>':
+					carts[ci].mydirection = 'v'
+					carts[ci].y++
+				case 'v':
+					carts[ci].mydirection = '>'
+					carts[ci].x++
+				}
+			case '-':
+				if c.mydirection == '<' {
+					carts[ci].x--
+				}
+				if c.mydirection == '>' {
+					carts[ci].x++
+				}
 			}
-			if c.mydirection == 'v' {
-				chart[c.x][c.y].y++
+			// Collision detection, have this cart collided
+			for ai, a := range carts {
+				if ci != ai {
+					if c.x == a.x && c.y == a.y {
+						if !firstcoll {
+							input_t_elapsed := time.Since(t_input)
+							log.Println("P1 Collision at:", c.x, c.y, "between", c, a, "Finished", input_t_elapsed, "after input")
+							t_input = time.Now()
+							firstcoll = true
+						}
+						deletionlist = append(deletionlist, ai)
+						deletionlist = append(deletionlist, ci)
+					}
+				}
 			}
-
-		case '+':
-			log.Println("turn")
-		case '/':
-			log.Println("cart turns right")
-		case '\':
-			log.Println("cart turns left")
-		case '-':
-			log.Println("Continue same dir")
 		}
-		// Collision detection, have this cart collided
 
-		for ai, a := range carts {
-			if c.x == a.x && c.y == a.y {
-				log.Println("Collision at:", c.x, c.y)
-				os.Exit(0)
+		// Deletion
+		if len(deletionlist) > 0 {
+			ox := carts[deletionlist[0]].x
+			oy := carts[deletionlist[0]].y
+			tx := carts[deletionlist[1]].x
+			ty := carts[deletionlist[1]].y
+			for ci, c := range carts {
+				if c.x == ox && c.y == oy {
+					carts = append(carts[:ci], carts[ci+1:]...)
+				}
 			}
+			for ci, c := range carts {
+				if c.x == tx && c.y == ty {
+					carts = append(carts[:ci], carts[ci+1:]...)
+				}
+			}
+			deletionlist = deletionlist[:0]
 		}
+
+		if len(carts) == 1 {
+			input_t_elapsed := time.Since(t_input)
+			log.Println("last cart is at", carts[0].x, carts[0].y, "Time:", input_t_elapsed)
+			os.Exit(0)
+		}
+
 	}
-	log.Println(carts)
+
 }
